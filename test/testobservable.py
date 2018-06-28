b059b460cd3b57f9674c08a55ffccd954436aa74
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2018-06-28 17:42:44 +0200

    bug fix

--- test/testobservable.py
+++ test/testobservable.py
@@ -5,7 +5,7 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
-from observer import Observer
+from observer.observer import Observer
 
 class TestObserver(unittest.TestCase):
 
