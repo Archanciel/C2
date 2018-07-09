4f8cb9a5c61643ed0fcc4a545cf40c5526382479
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2018-07-09 19:03:00 +0200

    deep improvment + now can be run in its own thread

--- test/testtimecounter.py
+++ test/testtimecounter.py
@@ -5,6 +5,7 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
+import time
 from utility.timecounter import TimeCounter
 
 class TestTimeCounter(unittest.TestCase):
@@ -12,120 +13,120 @@ class TestTimeCounter(unittest.TestCase):
         duration = 0
         counter = TimeCounter(durationSecond=duration)
 
-        dd, hh, mm, ss = counter.splitDurationSecond(duration)
+        counter.splitDurationSecond(duration)
 
-        self.assertEqual(dd, 0)
-        self.assertEqual(hh, 0)
-        self.assertEqual(mm, 0)
-        self.assertEqual(ss, 0)
+        self.assertEqual(counter.day, 0)
+        self.assertEqual(counter.hour, 0)
+        self.assertEqual(counter.minute, 0)
+        self.assertEqual(counter.second, 0)
 
 
     def testSplitDurationSecondSomeSS(self):
         duration = 40
         counter = TimeCounter(durationSecond=duration)
 
-        dd, hh, mm, ss = counter.splitDurationSecond(duration)
+        counter.splitDurationSecond(duration)
 
-        self.assertEqual(dd, 0)
-        self.assertEqual(hh, 0)
-        self.assertEqual(mm, 0)
-        self.assertEqual(ss, 40)
+        self.assertEqual(counter.day, 0)
+        self.assertEqual(counter.hour, 0)
+        self.assertEqual(counter.minute, 0)
+        self.assertEqual(counter.second, 40)
 
 
     def testSplitDurationSecondMMZeroSS(self):
         duration = 60
         counter = TimeCounter(durationSecond=duration)
 
-        dd, hh, mm, ss = counter.splitDurationSecond(duration)
+        counter.splitDurationSecond(duration)
 
-        self.assertEqual(dd, 0)
-        self.assertEqual(hh, 0)
-        self.assertEqual(mm, 1)
-        self.assertEqual(ss, 0)
+        self.assertEqual(counter.day, 0)
+        self.assertEqual(counter.hour, 0)
+        self.assertEqual(counter.minute, 1)
+        self.assertEqual(counter.second, 0)
 
 
     def testSplitDurationSecondMMSS(self):
         duration = 100
         counter = TimeCounter(durationSecond=duration)
 
-        dd, hh, mm, ss = counter.splitDurationSecond(duration)
+        counter.splitDurationSecond(duration)
 
-        self.assertEqual(dd, 0)
-        self.assertEqual(hh, 0)
-        self.assertEqual(mm, 1)
-        self.assertEqual(ss, 40)
+        self.assertEqual(counter.day, 0)
+        self.assertEqual(counter.hour, 0)
+        self.assertEqual(counter.minute, 1)
+        self.assertEqual(counter.second, 40)
 
 
     def testSplitDurationSecondHHZeroMMZeroSS(self):
         duration = 3600
         counter = TimeCounter(durationSecond=duration)
 
-        dd, hh, mm, ss = counter.splitDurationSecond(duration)
+        counter.splitDurationSecond(duration)
 
-        self.assertEqual(dd, 0)
-        self.assertEqual(hh, 1)
-        self.assertEqual(mm, 0)
-        self.assertEqual(ss, 0)
+        self.assertEqual(counter.day, 0)
+        self.assertEqual(counter.hour, 1)
+        self.assertEqual(counter.minute, 0)
+        self.assertEqual(counter.second, 0)
 
 
     def testSplitDurationSecondHHMMSS(self):
         duration = 3700
         counter = TimeCounter(durationSecond=duration)
 
-        dd, hh, mm, ss = counter.splitDurationSecond(duration)
+        counter.splitDurationSecond(duration)
 
-        self.assertEqual(dd, 0)
-        self.assertEqual(hh, 1)
-        self.assertEqual(mm, 1)
-        self.assertEqual(ss, 40)
+        self.assertEqual(counter.day, 0)
+        self.assertEqual(counter.hour, 1)
+        self.assertEqual(counter.minute, 1)
+        self.assertEqual(counter.second, 40)
 
 
     def testSplitDurationSecondDDZeroHHZeroMMZeroSS(self):
         duration = 86400
         counter = TimeCounter(durationSecond=duration)
 
-        dd, hh, mm, ss = counter.splitDurationSecond(duration)
+        counter.splitDurationSecond(duration)
 
-        self.assertEqual(dd, 1)
-        self.assertEqual(hh, 0)
-        self.assertEqual(mm, 0)
-        self.assertEqual(ss, 0)
+        self.assertEqual(counter.day, 1)
+        self.assertEqual(counter.hour, 0)
+        self.assertEqual(counter.minute, 0)
+        self.assertEqual(counter.second, 0)
 
 
     def testSplitDurationSecondDDZeroHHZeroMMSS(self):
         duration = 86404
         counter = TimeCounter(durationSecond=duration)
 
-        dd, hh, mm, ss = counter.splitDurationSecond(duration)
+        counter.splitDurationSecond(duration)
 
-        self.assertEqual(dd, 1)
-        self.assertEqual(hh, 0)
-        self.assertEqual(mm, 0)
-        self.assertEqual(ss, 4)
+        self.assertEqual(counter.day, 1)
+        self.assertEqual(counter.hour, 0)
+        self.assertEqual(counter.minute, 0)
+        self.assertEqual(counter.second, 4)
 
 
     def testSplitDurationSecondDDZeroHHMMSS(self):
         duration = 86504
         counter = TimeCounter(durationSecond=duration)
 
-        dd, hh, mm, ss = counter.splitDurationSecond(duration)
+        counter.splitDurationSecond(duration)
 
-        self.assertEqual(dd, 1)
-        self.assertEqual(hh, 0)
-        self.assertEqual(mm, 1)
-        self.assertEqual(ss, 44)
+        self.assertEqual(counter.day, 1)
+        self.assertEqual(counter.hour, 0)
+        self.assertEqual(counter.minute, 1)
+        self.assertEqual(counter.second, 44)
 
 
     def testSplitDurationSecondDDHMMSS(self):
         duration = 90104
         counter = TimeCounter(durationSecond=duration)
 
-        dd, hh, mm, ss = counter.splitDurationSecond(duration)
+        counter.splitDurationSecond(duration)
 
-        self.assertEqual(dd, 1)
-        self.assertEqual(hh, 1)
-        self.assertEqual(mm, 1)
-        self.assertEqual(ss, 44)
+        self.assertEqual(counter.day, 1)
+        self.assertEqual(counter.hour, 1)
+        self.assertEqual(counter.minute, 1)
+        self.assertEqual(counter.second, 44)
 
 
 if __name__ == '__main__':
--- utility/timecounter.py
+++ utility/timecounter.py
@@ -1,20 +1,30 @@
 import time
+from threading import Thread
+import sys
 
-class TimeCounter:
+
+class TimeCounter(Thread):
     MODE_COUNT_UP = 'mode_up'
     MODE_COUNT_DOWN = 'mode_down'
 
     def __init__(self, mode = MODE_COUNT_DOWN, intervalSecond = 1, durationSecond = 0):
-        if intervalSecond > 59:
-            raise ValueError('intervalSecond > 59 seconds not supported')
+        '''
 
+         In case a test list
+        is passed as parm, the printed time string is added to the list.
+        :param mode:
+        :param intervalSecond:
+        :param durationSecond:
+        :param testTimeStrList: is value not None, the printed time string is appended to it
+        '''
+        Thread.__init__(self)
         self.mode = mode
 
         if mode == TimeCounter.MODE_COUNT_DOWN:
             self.totalSecond = durationSecond
             self.modeString = 'Time remaining:'
-            self.second, self.minute, self.hour, self.day = self.splitDurationSecond(durationSecond)
-
+            self.splitDurationSecond(durationSecond)
+            self.initialCountDownTimeStr = ' (from {:02d} {:02d}:{:02d}:{:02d})'.format(self.day, self.hour, self.minute, self.second)
         else:
             self.totalSecond = 0
             self.modeString = 'Time elapsed:'
@@ -22,22 +32,39 @@ class TimeCounter:
             self.minute = 0
             self.hour = 0
             self.day = 0
+            self.initialCountDownTimeStr = ''
 
         self.intervalSecond = intervalSecond
         self.durationSecond = durationSecond
 
 
-    def splitDurationSecond(self, durationSecond):
-        dd = int(durationSecond / 86400)
-        durationSecond -= dd * 86400
-        hh = int(durationSecond / 3600)
-        durationSecond -= hh * 3600
-        mm = int(durationSecond / 60)
-        ss = durationSecond - mm * 60
+    def run(self):
+        active = True
 
-        return dd, hh, mm, ss
+        while active:
+            active = self.count()
+
+    def splitDurationSecond(self, durationSecond):
+        '''
+        Computes the time components corresponding to the passed duration in seconds.
+
+        :param durationSecond:
+        :return:
+        '''
+        self.day = int(durationSecond / 86400)
+        durationSecond -= self.day * 86400
+        self.hour = int(durationSecond / 3600)
+        durationSecond -= self.hour * 3600
+        self.minute = int(durationSecond / 60)
+        self.second = durationSecond - self.minute * 60
+
 
     def count(self):
+        '''
+        Print a DD HH:MM:SS count down or elapsed time string in the console.
+
+        :return:
+        '''
         time.sleep(self.intervalSecond)
 
         if self.mode == TimeCounter.MODE_COUNT_UP:
@@ -45,61 +72,52 @@ class TimeCounter:
         else:
             active = self.countDown()
 
-        timeStr = '\r{} {:02d} {:02d}:{:02d}:{:02d}'.format(self.modeString,self.day, self.hour, self.minute, self.second)
-        print(timeStr, end='')
+        timeStr = '\r{} {:02d} {:02d}:{:02d}:{:02d}{}'.format(self.modeString, self.day, self.hour, self.minute, self.second, self.initialCountDownTimeStr)
+#        print(timeStr, end='')
+        sys.stdout.write(timeStr)
+        sys.stdout.flush()
 
         return active
 
 
     def countUp(self):
+        self.splitDurationSecond(self.totalSecond)
         self.totalSecond += self.intervalSecond
-        self.second += self.intervalSecond
 
-        if self.second == 60:
-            self.second = 0
-            self.minute += 1
-        if self.minute == 60:
-            self.minute = 0
-            self.second = 0
-            self.hour += 1
-        if self.hour == 24:
-            self.hour = 0
-            self.second = 0
-            self.minute = 0
-            self.day += 1
-
-        if self.totalSecond >= self.durationSecond:
+        if self.totalSecond > self.durationSecond:
+            self.splitDurationSecond(self.totalSecond - 1)
             return False
         else:
             return True
 
 
     def countDown(self):
+        self.splitDurationSecond(self.totalSecond)
         self.totalSecond -= self.intervalSecond
-        self.second -= self.intervalSecond
 
-        if self.second == 60:
-            self.second = 0
-            self.minute += 1
-        if self.minute == 60:
-            self.minute = 0
-            self.second = 0
-            self.hour += 1
-        if self.hour == 24:
-            self.hour = 0
-            self.second = 0
-            self.minute = 0
-            self.day += 1
-
-        if self.totalSecond >= self.durationSecond:
+        if self.totalSecond < 0:
+            self.splitDurationSecond(self.totalSecond + 1)
             return False
         else:
             return True
 
 
 if __name__ == '__main__':
+    counter = TimeCounter(mode = TimeCounter.MODE_COUNT_DOWN, intervalSecond = 1, durationSecond = 10)
+    active = True
+
+    while active:
+        active = counter.count()
+
+    print()
+
     counter = TimeCounter(mode = TimeCounter.MODE_COUNT_UP, intervalSecond = 1, durationSecond = 10)
     active = True
 
     while active:
         active = counter.count()
+
+    print()
+
+    counter = TimeCounter(mode = TimeCounter.MODE_COUNT_DOWN, intervalSecond = 1, durationSecond = 10)
+    counter.start()
