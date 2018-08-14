517826475b1426fdf2ce81d84529a7df616128c6
Archanciel <jp.schnyder@gmail.com>
2018-08-14 21:38:59 +0200

    exploring

--- esspandamatplotlibpricevolume.py
+++ esspandamatplotlibpricevolume.py
@@ -4,16 +4,24 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 from matplotlib import style
+import matplotlib.dates as mdates
+import datetime
 
 style.use('ggplot')
 
-df = pd.read_csv('secondary-2018-07-11-15-32-11.csv', index_col=0)
+df = pd.read_csv('secondary-2018-08-12-21-32-56.csv', index_col=0, sep='\t')
+
 #df['100ma'] = df['Close'].rolling(window=100, min_periods=0).mean()
 print(df.head())
+l = df['TIMESTAMP (MS)'].tolist()
+xx = [datetime.datetime.fromtimestamp(x / 1000) for x in l]
+print(xx)
+myFmt = mdates.DateFormatter('%H:%M:%S')
+plt.gca().xaxis.set_major_formatter(myFmt)
 
 ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
 ax2v = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
-ax2v.set_ylim(0, 1 * df['VOLUME'].max())
+ax2v.set_ylim(0, 0.8 * df['VOLUME'].max())
 
 ax1.plot(df.index, df['PRICE'])
 #ax1.plot(df.index, df['100ma'])
